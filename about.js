6f3329f670657f8afbd50ed356fcd64f1ec75ddf
unknown <russel.tsang@gmail.com>
2018-12-10 16:23:51 -0500

    link change

---
+++ about.js
@@ -0,0 +1,10 @@
+var img = document.getElementsByTagName("img")[1];
+var missionp = document.getElementsByTagName("p")[0];
+var our_mission = document.querySelector(".jumbotron h1");
+
+window.setTimeout(function() {
+		img.style.opacity = "1";
+		missionp.style.opacity = "1";
+		our_mission.style.opacity = "1";
+
+}, 250);
