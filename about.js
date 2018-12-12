9a4c2663679fc60d2d0eeae3ac20eb224e9b7594
unknown <russel.tsang@gmail.com>
2018-12-12 02:54:23 -0500

    cleaned code

--- about.js
+++ about.js
@@ -6,16 +6,22 @@ var background = document.getElementsByClassName("background");
 
 window.setTimeout(function() {
 		img.style.opacity = "1";
-		background[0].style.left = "0";
+		if (background[0]) {
+			background[0].style.left = "0";
+		}
 }, 200);
 
 window.setTimeout(function() {
 		missionp.style.opacity = "1";
 		our_mission.style.opacity = "1";
 		hr.style.opacity = "1";
-		background[1].style.left = "0";
+		if (background[1]) {
+			background[1].style.left = "0";
+		}
 }, 400);
 
 window.setTimeout(function() {
-		background[2].style.left = "0";
+		if (background[2]) {
+			background[2].style.left = "0";
+		}
 }, 600);
