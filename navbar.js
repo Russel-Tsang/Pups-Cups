e47b01deceada369f942916ae6f92f0edac60084
unknown <russel.tsang@gmail.com>
2018-12-12 02:51:39 -0500

    Changed navbar color effects

--- navbar.js
+++ navbar.js
@@ -2,51 +2,23 @@ var get_started = document.getElementsByClassName("get_started")[0];
 var nav_links = document.querySelectorAll(".nav-link");
 var logo = document.getElementById("logo");
 
-$(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if(scroll < 600){
-        $('#navigation').css('background', 'black');
-
-        $('.nav-link').css('color', '#9ac8ed');
-
-        for (var i = 0; i < nav_links.length; i++) {
-            nav_links[i].addEventListener("mouseover", function(){
-                this.style.color = "white";
-            });
-        }
-        
-        for (var i = 0; i < nav_links.length; i++) {
-            nav_links[i].addEventListener("mouseout", function(){
-                this.style.color = "#9ac8ed";
-            });
-        }
-     
-    } else{
-        $('.navbar').css('background','transparent');
-
-        for (var i = 0; i < nav_links.length - 1; i++) {
-            nav_links[i].style.color = "white";
-        }
-        
-        for (var i = 0; i < nav_links.length - 1; i++) {
-            nav_links[i].addEventListener("mouseover", function(){
-                this.style.color = "#9ac8ed";
-            });
-        }
-        for (var i = 0; i < nav_links.length - 1; i++) {
-            nav_links[i].addEventListener("mouseout", function(){
-                this.style.color = "white";
-            });
-        }
-    }
-});
-
 for (var i = 0; i < nav_links.length; i++) {
     nav_links[i].style.color = "#9ac8ed";  
 }
 
+navColorChange();
 
+function navColorChange() {  // change nav link colors when on mouse-hover
+    for (var i = 0; i < nav_links.length; i++) {
+        nav_links[i].addEventListener("mouseover", function(){
+            this.style.color = "#52a0e0";
+        });
+    }
 
-
-
+    for (var i = 0; i < nav_links.length; i++) {
+        nav_links[i].addEventListener("mouseout", function(){
+            this.style.color = "#9ac8ed";
+        });
+    }
+}
 
